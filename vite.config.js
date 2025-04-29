0819b9db4691cf1af0dc33ba29463d60396e44b1
vnrnesia <102244488+vnrnesia@users.noreply.github.com>
2025-04-29 20:52:15 +0300

    Fix for Vercel

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.VITE_BASE_PATH || "/zinter"
+  base: process.env.VITE_BASE_PATH || "/zinter",
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
 })
