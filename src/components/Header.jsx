d34e8350f2f0adb3022a5411703bb9b6df355916
vnrnesia <102244488+vnrnesia@users.noreply.github.com>
2025-04-29 21:03:07 +0300

    Fix for vercel_4

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,5 @@
 import { useState } from "react";
 import ZinterLogo from "../assets/ZinterLogo.png";
-import Telegram from "../assets/Telegram/telegram.png";
 import Whatsapp from "../assets/whatsapp.png";
 
 export default function Header() {
