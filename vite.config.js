d8c454a7765a3b8203ebc35bff5fd40190b96e9d
vnrnesia <102244488+vnrnesia@users.noreply.github.com>
2025-05-02 11:31:38 +0300

    Fix Vite base path for production

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,14 @@
-// vite.config.js
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path'
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === "production" ? "/zinter" : "/",
+  base: "/", 
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
   },
-})
+});
