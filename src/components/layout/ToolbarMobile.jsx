a40fe2b618b221ca64882766f46f71d821b5a8a1
vnrnesia <102244488+vnrnesia@users.noreply.github.com>
2025-05-04 09:05:48 +0300

    Class fixes for some components.

--- src/components/layout/ToolbarMobile.jsx
+++ src/components/layout/ToolbarMobile.jsx
@@ -1,18 +1,12 @@
-import {
-  Home,
-  Package,
-  PhoneCall,
-  Menu,
-  X,
-  FileText,
-} from "lucide-react";
+import { Home, Package, PhoneCall, Menu, X, FileText } from "lucide-react";
 import { Link } from "react-router-dom";
 import { useState } from "react";
 import { motion, AnimatePresence } from "framer-motion";
 
 const icons = {
   whatsapp: "https://upload.wikimedia.org/wikipedia/commons/6/6b/WhatsApp.svg",
-  telegram: "https://upload.wikimedia.org/wikipedia/commons/8/82/Telegram_logo.svg",
+  telegram:
+    "https://upload.wikimedia.org/wikipedia/commons/8/82/Telegram_logo.svg",
   gmail: "https://upload.wikimedia.org/wikipedia/commons/4/4e/Gmail_Icon.png",
   phone: "https://cdn-icons-png.flaticon.com/512/597/597177.png",
 };
@@ -33,29 +27,43 @@ export default function ToolbarMobile() {
             <span>Главная</span>
           </Link>
         </li>
+
         <li>
-          <Link to="/warehouse" className="flex flex-col items-center text-gray-600">
-            <Package size={24} />
-            <span>Меню</span>
-          </Link>
-        </li>
-        <li>
-          <Link to="/contact" className="flex pl-2 flex-col items-center text-gray-600">
+          <Link
+            to="/contact"
+            className="flex pl-2 flex-col items-center text-gray-600"
+          >
             <FileText size={24} />
             <span>Заявка</span>
           </Link>
         </li>
+
         <li>
-          <button onClick={toggleMessageMenu} className="flex flex-col items-center text-gray-600">
+          <a
+            href="tel:+79178899457"
+            className="flex flex-col items-center text-gray-600"
+          >
+            <PhoneCall size={24} />
+            <span>Звонок</span>
+          </a>
+        </li>
+        <li>
+          <button
+            onClick={toggleMessageMenu}
+            className="flex flex-col items-center text-gray-600"
+          >
             {showMessageMenu ? <X size={24} /> : <Menu size={24} />}
             <span>Сообщение</span>
           </button>
         </li>
         <li>
-          <a href="tel:+79178899457" className="flex flex-col items-center text-gray-600">
-            <PhoneCall size={24} />
-            <span>Звонок</span>
-          </a>
+          <Link
+            to="/warehouse"
+            className="flex flex-col items-center text-gray-600"
+          >
+            <Package size={24} />
+            <span>Меню</span>
+          </Link>
         </li>
 
         <AnimatePresence>
@@ -67,11 +75,27 @@ export default function ToolbarMobile() {
               transition={{ duration: 0.2 }}
               className="absolute bottom-[70px] left-[61%] transform -translate-x-1/2 bg-white border rounded-xl shadow-xl p-2 z-50 flex flex-col items-center gap-3"
             >
-              <a href="https://wa.me/79178899457" target="_blank" rel="noreferrer">
-                <img src={icons.whatsapp} alt="WhatsApp" className="w-12 h-12" />
+              <a
+                href="https://wa.me/79178899457"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <img
+                  src={icons.whatsapp}
+                  alt="WhatsApp"
+                  className="w-12 h-12"
+                />
               </a>
-              <a href="https://t.me/yourtelegrambot" target="_blank" rel="noreferrer">
-                <img src={icons.telegram} alt="Telegram" className="w-10 h-10" />
+              <a
+                href="https://t.me/yourtelegrambot"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <img
+                  src={icons.telegram}
+                  alt="Telegram"
+                  className="w-10 h-10"
+                />
               </a>
               <a href="mailto:zinter@zinter.com">
                 <img src={icons.gmail} alt="Gmail" className="w-9 h-9" />
